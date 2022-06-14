e79dd62361939ec98d56911d5cbdec74887e54ec
mateega <mateega@upenn.edu>
2022-06-13 18:45:28 -0700

    splash screen

--- app/src/main/java/com/example/parstagram/SplashScreenActivity.java
+++ app/src/main/java/com/example/parstagram/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
     }
 }
