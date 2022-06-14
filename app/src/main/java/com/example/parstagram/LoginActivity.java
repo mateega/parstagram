ad8acbe84346156e6ab8d6a637058c5020674f16
mateega <mateega@upenn.edu>
2022-06-13 19:04:16 -0700

    login page look updated

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -26,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.getSupportActionBar().hide();
+
 
         if (ParseUser.getCurrentUser() != null) {
             goMainActivity();
